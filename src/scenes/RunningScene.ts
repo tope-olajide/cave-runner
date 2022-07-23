5b13c4c8ed52a54281f65bd7d0c62817ca9d577e
tope-olajide <tope-olajide@users.noreply.github.com>
2022-07-24 00:32:16 +0100

    Update RunningScene.ts

--- src/scenes/RunningScene.ts
+++ src/scenes/RunningScene.ts
@@ -1,6 +1,7 @@
 /* eslint-disable linebreak-style */
 import {
-  Scene, DirectionalLight, AmbientLight, Object3D, AnimationMixer, AnimationAction, Clock, Box3, LoopOnce,
+  Scene, DirectionalLight, AmbientLight, Object3D, AnimationMixer, AnimationAction, Clock,
+  Box3, Group, BoxGeometry, MeshPhongMaterial, Mesh, Vector3
 } from 'three';
 
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
@@ -43,6 +44,26 @@ export default class RunningScene extends Scene {
   private slidingAnimation !: AnimationAction;
 
   private sliderTimeout!: ReturnType<typeof setTimeout>;
+
+  private barrelObject = new Object3D();
+
+  private boxObject = new Object3D();
+
+  private spikeObject = new Object3D();
+
+  private obstacleArray: Group[] = [];
+
+  private currentObstacleOne = new Group();
+
+  private currentObstacleTwo = new Group();
+
+  private playerBox = new Mesh(new BoxGeometry(), new MeshPhongMaterial({ color: 0x0000ff }));
+
+  private playerBoxCollider = new Box3(new Vector3(), new Vector3());
+
+  private obstacleBox = new Box3(new Vector3(), new Vector3());
+
+  private obstacleBox2 = new Box3(new Vector3(), new Vector3());
 
   async load() {
     const ambient = new AmbientLight(0xFFFFFF, 2.5);
@@ -87,6 +108,36 @@ export default class RunningScene extends Scene {
     // remove the animation track that makes the player move forward when sliding
     slidingAnimationObject.animations[0].tracks.shift();
     this.slidingAnimation = this.animationMixer.clipAction(slidingAnimationObject.animations[0]);
+
+    this.barrelObject = await this.fbxLoader.loadAsync('../../assets/models/barrel.fbx');
+    this.boxObject = await this.fbxLoader.loadAsync('../../assets/models/box.fbx');
+    this.spikeObject = await this.fbxLoader.loadAsync('../../assets/models/spike.fbx');
+
+    this.createLeftJumpObstacle();
+
+    this.createLeftJumpObstacle();
+
+    this.createCenterJumpObstacle();
+
+    this.createRightJumpObstacle();
+
+    this.createRightCenterObstacle();
+
+    this.createLeftSlideObstacle();
+
+    this.createCenterRightObstacle();
+
+    this.createLeftCenterObstacle();
+
+    this.createLeftRightObstacle();
+
+    this.createCenterSlideObstacle();
+
+    this.createRightSlideObstacle();
+
+    this.playerBox.scale.set(50, 200, 20);
+    this.playerBox.position.set(0, 90, 0);
+    this.player.add(this.playerBox);
   }
 
   initialize() {
@@ -121,10 +172,38 @@ export default class RunningScene extends Scene {
       this.woodenCaveClone.position.z = this.woodenCave.position.z - this.caveSize;
     }
     TWEEN.update();
+
+    this.spawnObstacle();
+
+    this.playerBoxCollider.setFromObject(this.playerBox);
+
+    this.detectCollisionWithObstacles();
   }
 
   hide() {
 
+  }
+
+  private gameOver() {
+    console.log('game over');
+  }
+
+  private detectCollisionWithObstacles() {
+   //  this.playerBoxCollider.setFromObject(this.playerBox);
+
+    for (let i = 0; i < this.currentObstacleOne.children.length; i += 1) {
+      this.obstacleBox.setFromObject(this.currentObstacleOne.children[i]);
+      if (this.playerBoxCollider.intersectsBox(this.obstacleBox)) {
+        this.gameOver();
+      }
+    }
+    for (let i = 0; i < this.currentObstacleTwo.children.length; i += 1) {
+      this.obstacleBox2.setFromObject(this.currentObstacleTwo.children[i]);
+
+      if (this.playerBoxCollider.intersectsBox(this.obstacleBox2)) {
+        this.gameOver();
+      }
+    }
   }
 
   private moveLeft() {
@@ -217,5 +296,243 @@ export default class RunningScene extends Scene {
         this.isSliding = false;
       }, 800);
     }
+  }
+
+  private createRandomObstacle() {
+    let randomNum = Math.floor(Math.random() * this.obstacleArray.length);
+
+    while (this.obstacleArray[randomNum] === this.currentObstacleOne
+      || this.obstacleArray[randomNum] === this.currentObstacleTwo) {
+      randomNum = Math.floor(Math.random() * this.obstacleArray.length);
+    }
+    return this.obstacleArray[randomNum];
+  }
+
+  private spawnObstacle() {
+    if (!this.currentObstacleOne.visible) {
+      this.currentObstacleOne.visible = true;
+    }
+
+    if (!this.currentObstacleTwo.visible) {
+      this.currentObstacleTwo.visible = true;
+      this.currentObstacleTwo.position.z = this.currentObstacleOne.position.z - 450;
+    }
+
+    this.currentObstacleOne.position.z += this.speed * this.delta;
+    this.currentObstacleTwo.position.z += this.speed * this.delta;
+
+    if (this.currentObstacleOne.position.z > -40) {
+      this.currentObstacleOne.visible = false;
+      this.currentObstacleOne.position.z = -1100;
+      this.currentObstacleOne = this.createRandomObstacle();
+    }
+
+    if (this.currentObstacleTwo.position.z > -40) {
+      this.currentObstacleTwo.visible = false;
+      this.currentObstacleTwo.position.z = this.currentObstacleOne.position.z - 450;
+      this.currentObstacleTwo = this.createRandomObstacle();
+    }
+  }
+
+  private createLeftJumpObstacle() {
+    const meshGroup = new Group();
+    const mesh = this.barrelObject.clone();
+    mesh.scale.set(0.03, 0.03, 0.03);
+    mesh.position.set(0, -25, 0);
+    meshGroup.add(mesh);
+    const mesh2 = this.barrelObject.clone();
+    mesh2.scale.set(0.03, 0.03, 0.03);
+    mesh2.position.set(20, -25, 0);
+    meshGroup.add(mesh2);
+    const mesh3 = this.spikeObject.clone();
+    mesh3.scale.set(0.06, 0.06, 0.06);
+    mesh3.position.set(-20, -31, 0);
+    meshGroup.add(mesh3);
+    meshGroup.position.set(0, 0, -800);
+    this.add(meshGroup);
+    meshGroup.visible = false;
+    this.obstacleArray.push(meshGroup);
+  }
+
+  private createCenterJumpObstacle() {
+    const meshGroup = new Group();
+    const mesh = this.barrelObject.clone();
+    mesh.scale.set(0.03, 0.03, 0.03);
+    mesh.position.set(-20, -25, 0);
+    meshGroup.add(mesh);
+    const mesh2 = this.barrelObject.clone();
+    mesh2.scale.set(0.03, 0.03, 0.03);
+    mesh2.position.set(20, -25, 0);
+    meshGroup.add(mesh2);
+    const mesh3 = this.spikeObject.clone();
+    mesh3.scale.set(0.06, 0.06, 0.06);
+    mesh3.position.set(0, -31, 0);
+    meshGroup.add(mesh3);
+    meshGroup.position.set(0, 0, -1200);
+    this.add(meshGroup);
+    meshGroup.visible = false;
+    this.obstacleArray.push(meshGroup);
+  }
+
+  private createRightJumpObstacle() {
+    const meshGroup = new Group();
+    const mesh = this.barrelObject.clone();
+    mesh.scale.set(0.03, 0.03, 0.03);
+    mesh.position.set(-20, -25, 0);
+    meshGroup.add(mesh);
+    const mesh2 = this.barrelObject.clone();
+    mesh2.scale.set(0.03, 0.03, 0.03);
+    mesh2.position.set(0, -25, 0);
+    meshGroup.add(mesh2);
+    const mesh3 = this.spikeObject.clone();
+    mesh3.scale.set(0.06, 0.06, 0.06);
+    mesh3.position.set(20, -31, 0);
+    meshGroup.add(mesh3);
+    meshGroup.position.set(0, 0, -1200);
+    this.add(meshGroup);
+    meshGroup.visible = false;
+    this.obstacleArray.push(meshGroup);
+  }
+
+  private createRightCenterObstacle() {
+    const meshGroup = new Group();
+    const mesh = this.barrelObject.clone();
+    mesh.scale.set(0.03, 0.03, 0.03);
+    mesh.position.set(0, -25, 0);
+    meshGroup.add(mesh);
+    const mesh2 = this.barrelObject.clone();
+    mesh2.scale.set(0.03, 0.03, 0.03);
+    mesh2.position.set(20, -25, 0);
+    meshGroup.add(mesh2);
+    meshGroup.position.set(0, 0, -1200);
+    this.add(meshGroup);
+    meshGroup.visible = false;
+    this.obstacleArray.push(meshGroup);
+  }
+
+  private createLeftCenterObstacle() {
+    const meshGroup = new Group();
+    const mesh = this.barrelObject.clone();
+    mesh.scale.set(0.03, 0.03, 0.03);
+    mesh.position.set(-20, -25, 0);
+    meshGroup.add(mesh);
+    const mesh2 = this.barrelObject.clone();
+    mesh2.scale.set(0.03, 0.03, 0.03);
+    mesh2.position.set(0, -25, 0);
+    meshGroup.add(mesh2);
+    meshGroup.position.set(0, 0, -1200);
+    this.add(meshGroup);
+    meshGroup.visible = false;
+    this.obstacleArray.push(meshGroup);
+  }
+
+  private createLeftRightObstacle() {
+    const meshGroup = new Group();
+    const mesh = this.barrelObject.clone();
+    mesh.scale.set(0.03, 0.03, 0.03);
+    mesh.position.set(-20, -25, 0);
+    meshGroup.add(mesh);
+    const mesh2 = this.barrelObject.clone();
+    mesh2.scale.set(0.03, 0.03, 0.03);
+    mesh2.position.set(20, -25, 0);
+    meshGroup.add(mesh2);
+    meshGroup.position.set(0, 0, -1200);
+    this.add(meshGroup);
+    meshGroup.visible = false;
+    this.obstacleArray.push(meshGroup);
+  }
+
+  private createCenterRightObstacle() {
+    const meshGroup = new Group();
+    const mesh = this.barrelObject.clone();
+    mesh.scale.set(0.031, 0.031, 0.031);
+    mesh.position.set(-20, -25, 0);
+    meshGroup.add(mesh);
+    const mesh2 = this.barrelObject.clone();
+    mesh2.scale.set(0.03, 0.03, 0.03);
+    mesh2.position.set(20, -25, 0);
+    meshGroup.add(mesh2);
+    meshGroup.position.set(0, 0, -1200);
+    this.add(meshGroup);
+    meshGroup.visible = false;
+    this.obstacleArray.push(meshGroup);
+  }
+
+  private createCenterSlideObstacle() {
+    const meshGroup = new Group();
+    const geometry = new BoxGeometry();
+    const material = new MeshPhongMaterial({ color: 'brown' });
+    const plank = new Mesh(geometry, material);
+    meshGroup.add(plank);
+    plank.position.set(0, -20, 0);
+    plank.scale.set(40, 0.5, 7);
+    const mesh = this.barrelObject.clone();
+    mesh.scale.set(0.03, 0.03, 0.03);
+    mesh.position.set(-20, -25, 0);
+    meshGroup.add(mesh);
+    const mesh2 = this.barrelObject.clone();
+    mesh2.scale.set(0.03, 0.03, 0.03);
+    mesh2.position.set(20, -25, 0);
+    meshGroup.add(mesh2);
+    const mesh3 = this.boxObject.clone();
+    mesh3.scale.set(4, 2, 2);
+    mesh3.position.set(0, -19, 3);
+    meshGroup.add(mesh3);
+    meshGroup.position.set(0, 0, -1200);
+    this.add(meshGroup);
+    meshGroup.visible = false;
+    this.obstacleArray.push(meshGroup);
+  }
+
+  private createRightSlideObstacle() {
+    const meshGroup = new Group();
+    const geometry = new BoxGeometry();
+    const material = new MeshPhongMaterial({ color: 'brown' });
+    const plank = new Mesh(geometry, material);
+    meshGroup.add(plank);
+    plank.position.set(20, -20, 0);
+    plank.scale.set(40, 0.5, 7);
+    const mesh = this.barrelObject.clone();
+    mesh.scale.set(0.03, 0.03, 0.03);
+    mesh.position.set(-20, -25, 0);
+    meshGroup.add(mesh);
+    const mesh2 = this.barrelObject.clone();
+    mesh2.scale.set(0.03, 0.03, 0.03);
+    mesh2.position.set(0, -25, 0);
+    meshGroup.add(mesh2);
+    const mesh3 = this.boxObject.clone();
+    mesh3.scale.set(4, 2, 2);
+    mesh3.position.set(20, -19, 3);
+    meshGroup.add(mesh3);
+    meshGroup.position.set(0, 0, -1200);
+    this.add(meshGroup);
+    meshGroup.visible = false;
+    this.obstacleArray.push(meshGroup);
+  }
+
+  private createLeftSlideObstacle() {
+    const meshGroup = new Group();
+    const geometry = new BoxGeometry();
+    const material = new MeshPhongMaterial({ color: 'brown' });
+    const plank = new Mesh(geometry, material);
+    meshGroup.add(plank);
+    plank.position.set(-20, -20, 0);
+    plank.scale.set(40, 0.5, 7);
+    const mesh = this.barrelObject.clone();
+    mesh.scale.set(0.03, 0.03, 0.03);
+    mesh.position.set(20, -25, 0);
+    meshGroup.add(mesh);
+    const mesh2 = this.barrelObject.clone();
+    mesh2.scale.set(0.03, 0.03, 0.03);
+    mesh2.position.set(0, -25, 0);
+    meshGroup.add(mesh2);
+    const mesh3 = this.boxObject.clone();
+    mesh3.scale.set(4, 2, 2);
+    mesh3.position.set(-20, -19, 3);
+    meshGroup.add(mesh3);
+    meshGroup.position.set(0, 0, -1200);
+    this.add(meshGroup);
+    meshGroup.visible = false;
+    this.obstacleArray.push(meshGroup);
   }
 }
