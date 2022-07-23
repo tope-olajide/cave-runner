ab462c6ecbba34e058249b3633787039bc3d7270
tope-olajide <tope-olajide@users.noreply.github.com>
2022-07-23 16:09:59 +0100

    Update RunningScene.ts

--- src/scenes/RunningScene.ts
+++ src/scenes/RunningScene.ts
@@ -1,8 +1,10 @@
 import {
-  Scene, DirectionalLight, AmbientLight, Object3D, AnimationMixer, AnimationAction, Clock,
+  Scene, DirectionalLight, AmbientLight, Object3D, AnimationMixer, AnimationAction, Clock, Box3, LoopOnce,
 } from 'three';
 
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+
+import TWEEN, { Tween } from '@tweenjs/tween.js';
 
 export default class RunningScene extends Scene {
   private fbxLoader = new FBXLoader();
@@ -18,6 +20,22 @@ export default class RunningScene extends Scene {
   private clock = new Clock();
 
   private delta = 0;
+
+  private woodenCaveClone = new Object3D();
+
+  private caveSize = 0;
+
+  private speed = 220;
+
+  private currentAnimation!: AnimationAction;
+
+  private jumpingAnimation!: AnimationAction;
+
+  private isJumping = false;
+
+  private jumpingUp!: Tween<any>;
+
+  private jumpingDown!: Tween<any>;
 
   async load() {
     const ambient = new AmbientLight(0xFFFFFF, 2.5);
@@ -45,10 +63,31 @@ export default class RunningScene extends Scene {
     this.animationMixer = new AnimationMixer(this.player);
     this.runningAnimation = this.animationMixer.clipAction(runningAnimationObject.animations[0]);
     this.runningAnimation.play();
+
+    this.woodenCaveClone = this.woodenCave.clone();
+    const caveBox = new Box3().setFromObject(this.woodenCave);
+    this.caveSize = caveBox.max.z - caveBox.min.z - 1;
+    this.woodenCaveClone.position.z = this.woodenCave.position.z + this.caveSize;
+    this.add(this.woodenCaveClone);
+
+    this.currentAnimation = this.runningAnimation;
+
+    const jumpingAnimationObject = await this.fbxLoader.loadAsync('./assets/animations/xbot@jumping.fbx');
+
+    this.jumpingAnimation = this.animationMixer.clipAction(jumpingAnimationObject.animations[0]);
   }
 
   initialize() {
-
+    document.onkeydown = (e) => {
+      if (e.key === 'ArrowLeft') {
+        this.moveLeft();
+      } if (e.key === 'ArrowRight') {
+        this.moveRight();
+      }
+      if (e.key === 'ArrowUp') {
+        this.jump();
+      }
+    };
   }
 
   update() {
@@ -56,9 +95,83 @@ export default class RunningScene extends Scene {
       this.delta = this.clock.getDelta();
       this.animationMixer.update(this.delta);
     }
+    this.woodenCave.position.z += this.speed * this.delta;
+    this.woodenCaveClone.position.z += this.speed * this.delta;
+
+    if (this.woodenCave.position.z > 600) {
+      this.woodenCave.position.z = this.woodenCaveClone.position.z - this.caveSize;
+    }
+
+    if (this.woodenCaveClone.position.z > 600) {
+      this.woodenCaveClone.position.z = this.woodenCave.position.z - this.caveSize;
+    }
+    TWEEN.update();
   }
 
   hide() {
 
+  }
+
+  private moveLeft() {
+    if (this.player.position.x !== -18) {
+      const tweenLeft = new TWEEN.Tween(this.player.position)
+        .to({ x: this.player.position.x - 18 }, 200)
+        .easing(TWEEN.Easing.Quadratic.Out)
+        .onUpdate(() => {
+          this.player.rotation.y = -140 * (Math.PI / 180);
+          if (this.player.position.x <= -18) {
+            this.player.position.x = -18;
+          }
+        })
+        .onComplete(() => {
+          this.player.rotation.y = 180 * (Math.PI / 180);
+        });
+      tweenLeft.start();
+    }
+  }
+
+  private moveRight() {
+    if (this.player.position.x !== 18) {
+      this.player.rotation.y = 140 * (Math.PI / 180);
+      const tweenRight = new Tween(this.player.position)
+        .to({ x: this.player.position.x + 18 }, 200)
+        .easing(TWEEN.Easing.Quadratic.Out)
+        .onUpdate(() => {
+          if (this.player.position.x >= 18) {
+            this.player.position.x = 18;
+          }
+        })
+        .onComplete(() => {
+          this.player.rotation.y = 180 * (Math.PI / 180);
+        });
+      tweenRight.start();
+    }
+  }
+
+  private jump() {
+    if (!this.isJumping) {
+      this.isJumping = true;
+      this.currentAnimation.stop();
+
+      this.currentAnimation = this.jumpingAnimation;
+      this.currentAnimation.reset();
+      this.currentAnimation.setLoop(1, 1);
+      this.currentAnimation.clampWhenFinished = true;
+      this.currentAnimation.play();
+      this.animationMixer.addEventListener('finished', () => {
+        this.currentAnimation.crossFadeTo(this.runningAnimation, 0.1, false).play();
+        this.currentAnimation = this.runningAnimation;
+      });
+
+      this.jumpingUp = new Tween(this.player.position).to({ y: this.player.position.y += 20 }, 400);
+      this.jumpingDown = new Tween(this.player.position)
+        .to({ y: this.player.position.y -= 20 }, 500);
+      this.jumpingUp.chain(this.jumpingDown);
+      this.jumpingUp.start();
+      this.jumpingDown.onComplete(() => {
+        this.isJumping = false;
+        this.player.position.y = -35;
+      });
+    }
   }
 }
