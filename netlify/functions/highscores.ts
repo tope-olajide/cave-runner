ad27287a05d1dd1751d001f92b592fd4afd23541
tope-olajide <tope-olajide@users.noreply.github.com>
2022-08-01 07:14:52 +0100

    Update highscores.ts

--- netlify/functions/highscores.ts
+++ netlify/functions/highscores.ts
@@ -1,6 +1,5 @@
 import { Handler } from '@netlify/functions';
 import mysql from 'mysql2/promise';
-import authenticateToken from './authenticateToken';
 
 require('dotenv').config();
 
@@ -8,21 +7,18 @@ const handler: Handler = async (event) => {
   if (event.httpMethod !== 'GET') {
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
-  const token = event.headers.authorization;
-  const user = authenticateToken(token);
-  console.log(user);
   try {
     const { DATABASE_URL } = process.env;
     const connection = await mysql.createConnection(DATABASE_URL);
-    const [rows] = await connection.execute(
+    const [highscores] = await connection.execute(
       'SELECT username, scores, country FROM players ORDER BY scores DESC LIMIT 10',
     );
     return {
       statusCode: 200,
-      body: JSON.stringify(rows),
+      body: JSON.stringify({ highscores }),
     };
   } catch (error) {
-    return { statusCode: 500, body: String(error), message: 'unable to fetch scores' };
+    return { statusCode: 500, body: JSON.stringify({ message: 'Unable to fetch High Scores!' }) };
   }
 };
 
